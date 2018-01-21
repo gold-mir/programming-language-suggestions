45f085d99861cbdc7c19ed2404e4df82d8cde57f
gold-mir <miranda.keith.42@gmail.com>
2018-01-20 16:46:56 -0800

    submit button now displays a language based on your answers

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,35 @@ $(document).ready(function(){
   $(".next-button").click(function(){
     var currentQuestion = $(this).parent();
     currentQuestion.fadeOut('fast', function(){
-      console.log(currentQuestion.next().index());
       currentQuestion.next().fadeIn();
     });
+  });
 
+  $("#language-selector-quiz").submit(function(event){
+    event.preventDefault();
+    var answers = $("#language-selector-quiz input:checked");
+    var totals = {
+      java: 0,
+      ruby: 0,
+      csharp: 0,
+    }
+    answers.each(function(answerIndex, item){
+      var combinedAnswerString = $(item).val();
+      console.log(item);
+      combinedAnswerString.split(",").forEach(function(answerString){
+        var answer = answerString.split(":");
+        totals[answer[0]] += parseInt(answer[1]);
+      });
+    });
+
+    if(totals.ruby > totals.java && totals.ruby > totals.csharp){
+      $("#ruby-info").fadeIn();
+    } else if(totals.java > totals.csharp){
+      $("#java-info").fadeIn();
+    } else if(totals.csharp > totals.java){
+      $("#csharp-info").fadeIn();
+    } else {
+      $("#any-info").fadeIn();
+    }
   });
 });
